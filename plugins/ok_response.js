4718ad942bfa2ad60085f9e26fc030ded038b6bd
Brendan McShane <brendanmcs7@gmail.com>
2025-03-22 14:36:30 -0400

    Update

--- plugins/ok_response.js
+++ plugins/ok_response.js
@@ -11,3 +11,13 @@ exports.hook_queue = function (next, connection) {
     this.loginfo("Received an email, responding with OK.");
     return next(OK, "OK");
 };
+
+exports.hook_rcpt = function (next, connection, params) {
+    // params[0] is the recipient email address
+    var recipient = params[0];
+
+    this.loginfo("Received RCPT TO: " + recipient);
+
+    // Always accept the recipient, no matter what
+    return next();
+};
